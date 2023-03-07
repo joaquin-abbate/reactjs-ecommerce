09229218d70415a307186fb938eb07b74727651c
joaquin abbate <joaquinabbate79@gmail.com>
2023-03-07 11:17:09 -0300

    Se agregó la parte de CommonSection, y en lo demas arreglos de rutas

--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -4,22 +4,42 @@ import "../../styles/product-card.css";
 import { Col } from "reactstrap";
 import { Link } from "react-router-dom";
 
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../redux/slices/cartSlice";
+import { toast } from "react-toastify";
+
 const ProductCard = ({ item }) => {
+  const dispatch = useDispatch();
+
+  const addToCart = () => {
+    dispatch(
+      cartActions.addItem({
+        id: item.id,
+        productName: item.productName,
+        image: item.imgUrl,
+        price: item.price,
+      })
+    );
+    toast.success("Producto añadido al carrito");
+  };
+
   return (
     <Col lg="3" md="4" className="mb-2">
       <div className="product__item">
-        <div className="product__img">
-          <motion.img whileHover={{ scale: 0.9 }} src={item.imgUrl} alt="" />
-        </div>
+        <Link to={`../shop/${item.id}`}>
+          <div className="product__img">
+            <motion.img whileHover={{ scale: 0.9 }} src={item.imgUrl} alt="" />
+          </div>
+        </Link>
         <div className="p-2 product__info">
           <h3 className="product__name">
-            <Link to={`/shop/${item.id}`}>{item.productName}</Link>
+            <Link to={`./${item.id}`}>{item.productName}</Link>
           </h3>
           <span>{item.category}</span>
         </div>
         <div className="product__card-bottom d-flex align-items-center justify-content-between p-2">
           <span className="price">${item.price}</span>
-          <motion.span whileTap={{ scale: 1.2 }}>
+          <motion.span whileTap={{ scale: 1.2 }} onClick={addToCart}>
             <i class="ri-add-line"></i>
           </motion.span>
         </div>
--- src/components/UI/ProductList.jsx
+++ src/components/UI/ProductList.jsx
@@ -3,8 +3,8 @@ import ProductCard from "./ProductCard";
 const ProductList = ({ data }) => {
   return (
     <>
-      {data?.map((item) => (
-        <ProductCard item={item} />
+      {data?.map((item, index) => (
+        <ProductCard item={item} key={index} />
       ))}
     </>
   );
