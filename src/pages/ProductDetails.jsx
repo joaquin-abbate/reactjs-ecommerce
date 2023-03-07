d7edbe08f4b285a4efb11a2a13762de0ba4e165d
joaquin abbate <joaquinabbate79@gmail.com>
2023-03-07 11:19:13 -0300

    Se creó el apartado donde se muestra el producto detallado

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,190 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+
+import { Container, Row, Col } from "reactstrap";
+import { useParams } from "react-router-dom";
+import products from "../assets/data/products";
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import "../styles/product-details.css";
+import { motion } from "framer-motion";
+import ProductsList from "../components/UI/ProductList";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../redux/slices/cartSlice";
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const reviewUser = useRef("");
+  const reviewMsg = useRef("");
+  const dispatch = useDispatch();
+
+  const [tab, setTab] = useState("desc");
+  const [rating, setRating] = useState(null);
+  const { id } = useParams();
+  const product = products.find((item) => item.id === id);
+
+  const {
+    imgUrl,
+    productName,
+    price,
+    avgRating,
+    reviews,
+    description,
+    shortDesc,
+    category,
+  } = product;
+
+  const relatedProducts = products.filter((item) => item.category === category);
+
+  const sumbitHandler = (e) => {
+    e.preventdefault();
+    const reviewUserName = reviewUser.current.value;
+    const reviewUserMsg = reviewMsg.current.value;
+  };
+
+  return (
+    <Helmet title={productName}>
+      <CommonSection title={productName} />
+      <section className="pt-0">
+        <Container>
+          <Row>
+            <Col lg="6">
+              <img src={imgUrl} alt="" />
+            </Col>
+
+            <Col lg="6">
+              <div className="products__details">
+                <h2>{productName}</h2>
+                <div className="product__rating d-flex align-items-center gap-5 mb-3">
+                  <div>
+                    <span>
+                      <i class="ri-star-fill"></i>
+                    </span>
+                    <span>
+                      <i class="ri-star-fill"></i>
+                    </span>
+                    <span>
+                      <i class="ri-star-fill"></i>
+                    </span>
+                    <span>
+                      <i class="ri-star-fill"></i>
+                    </span>
+                    <span>
+                      {" "}
+                      <i class="ri-star-half-fill"></i>
+                    </span>
+                  </div>
+
+                  <p>
+                    (<span>{avgRating}</span> Reseñas)
+                  </p>
+                </div>
+                <div className="d-flex align-items-center gap-5">
+                  <span className="product__price">${price}</span>
+                  <span>Categoria: {category.toUpperCase()}</span>
+                </div>
+                <p className="mt-3">{shortDesc}</p>
+
+                <motion.button whileTap={{ scale: 1.2 }} className="buy__btn">
+                  Agregar al carrito
+                </motion.button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <div className="tab__wrapper d-flex align-items-center gap-5">
+                <h6
+                  className={`${tab === "desc" ? "active__tab" : ""}`}
+                  onClick={() => setTab("desc")}
+                >
+                  Descripción
+                </h6>
+                <h6
+                  className={`${tab === "rev" ? "active__tab" : ""}`}
+                  onClick={() => setTab("rev")}
+                >
+                  Reseñas ({reviews.length})
+                </h6>
+              </div>
+
+              {tab === "desc" ? (
+                <div className="tab__content mt-5">
+                  <p>{description}</p>
+                </div>
+              ) : (
+                <div className="product__review mt-5">
+                  <div className="review__wrapper">
+                    <ul>
+                      {reviews?.map((item, index) => (
+                        <li key={index} className="mb-4">
+                          <h6>Joaquin Abbate</h6>
+                          <span>{item.rating}(Calificacion)</span>
+                          <p>{item.text}</p>
+                        </li>
+                      ))}
+                    </ul>
+
+                    <div className="review__form">
+                      <h4>Dejanos tu experiencia</h4>
+                      <form action="" onSubmit={sumbitHandler}>
+                        <div className="form__group">
+                          <input
+                            type="text"
+                            placeholder="Nombre"
+                            ref={reviewUser}
+                          />
+                        </div>
+
+                        <div className="form__group d-flex align-items-center gap-5">
+                          <span onClick={() => setRating(1)}>
+                            1<i class="ri-star-s-fill"></i>
+                          </span>
+                          <span onClick={() => setRating(2)}>
+                            2<i class="ri-star-s-fill"></i>
+                          </span>
+                          <span onClick={() => setRating(3)}>
+                            3<i class="ri-star-s-fill"></i>
+                          </span>
+                          <span onClick={() => setRating(4)}>
+                            4<i class="ri-star-s-fill"></i>
+                          </span>
+                          <span onClick={() => setRating(5)}>
+                            5<i class="ri-star-s-fill"></i>
+                          </span>
+                        </div>
+
+                        <div className="form__group">
+                          <textarea
+                            ref={reviewMsg}
+                            rows={4}
+                            type="text"
+                            placeholder="Mensaje de reseña"
+                          />
+                        </div>
+
+                        <button type="sumbit" className="buy__btn">
+                          Enviar
+                        </button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </Col>
+
+            <Col lg="12">
+              <h2 className="relatedTitle">Tambien te puede interesar</h2>
+            </Col>
+            <ProductsList data={relatedProducts} />
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default ProductDetails;
