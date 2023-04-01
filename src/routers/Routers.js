8b9b47f1d64ab3087e64189706332299979d9520
joaquin abbate <joaquinabbate79@gmail.com>
2023-04-01 19:45:02 -0300

    Creacion de rutas para productos filtrados

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -8,6 +8,11 @@ import ProductDetails from "../pages/ProductDetails";
 import Shop from "../pages/Shop";
 import Singup from "../pages/Singup";
 import ProtectedRoute from "./ProtectedRoute";
+import Sofa from "../pages/categories/sofa";
+import Mobile from "../pages/categories/mobile";
+import Chair from "../pages/categories/chair";
+import Wireless from "../pages/categories/wireless";
+import Watch from "../pages/categories/watch";
 
 const Routers = () => {
   return (
@@ -26,6 +31,11 @@ const Routers = () => {
       />
       <Route path="login" element={<Login />} />
       <Route path="shop/" element={<Shop />} />
+      <Route path="shop/sofa" element={<Sofa />} />
+      <Route path="shop/mobile" element={<Mobile />} />
+      <Route path="shop/chair" element={<Chair />} />
+      <Route path="shop/wireless" element={<Wireless />} />
+      <Route path="shop/watch" element={<Watch />} />
       <Route path="shop/:id" element={<ProductDetails />} />
       <Route path="singup" element={<Singup />} />
     </Routes>
